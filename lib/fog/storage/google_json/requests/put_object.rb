8f48aecf9328a9df40683856f1a666c4236403bf
Dawid Janczak <dawid.janczak@gmail.com>
2017-05-03 21:12:44 +0200

    Remove commented out code

--- lib/fog/storage/google_json/requests/put_object.rb
+++ lib/fog/storage/google_json/requests/put_object.rb
@@ -36,14 +36,6 @@ module Fog
             mime_type = Fog::Storage.parse_data(data)[:headers]["Content-Type"]
           end
 
-          # media = ::Google::APIClient::UploadIO.new(data, mime_type, object_name)
-          # api_method = @storage_json.objects.insert
-          # parameters = {
-            # "uploadType" => "multipart",
-            # "bucket" => bucket_name,
-            # "name" => object_name
-          # }
-
           body_object = {
             :contentType => mime_type,
             :contentEncoding => options["contentEncoding"]
@@ -71,7 +63,6 @@ module Fog
                                       name: object_name,
                                       predefined_acl: options['predefinedAcl'],
                                       upload_source: data.path)
-          # request(api_method, parameters, body_object = body_object, media = media)
         end
       end
 
